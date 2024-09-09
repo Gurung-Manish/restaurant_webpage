8f496cc48beb0a9f77cce335866e4b0184587ff3
Gurung-Manish <manishgrg496@gmail.com>
2024-09-09 19:15:18 +0100

    importing in gallerrry

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import './Gallery.css';
-
-const Gallery = () => (
-  <div>
-    Gallery
-  </div>
-);
+import {
+  BsInstagram,
+  BsArrowLeftShort,
+  BsArrowRightShort,
+} from "react-icons/bs";
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
+import "./Gallery.css";
+const Gallery = () => <div>Gallery</div>;
 
 export default Gallery;
